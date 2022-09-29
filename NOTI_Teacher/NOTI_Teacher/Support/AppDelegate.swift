dc111d4f9aef4918550d264e43310c8ffe3a0b67
yunkyung-Hwang <dudhkd98@naver.com>
2022-09-29 22:25:59 +0900

    NOTI-8/기본 TabBar 구현

--- NOTI_Teacher/NOTI_Teacher/Support/AppDelegate.swift
+++ NOTI_Teacher/NOTI_Teacher/Support/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow()
         
-        window?.rootViewController = HomeVC()
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
         
         return true
