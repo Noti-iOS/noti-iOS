3a6178a1fe7fcbb876c2645fe25f0c4b0f5728ac
yunkyung-Hwang <dudhkd98@naver.com>
2022-10-04 15:13:59 +0900

    NOTI-9/ClassProgressCV 구현

--- NOTI_Teacher/NOTI_Teacher/Screen/Home/ViewController/HomeVC.swift
+++ NOTI_Teacher/NOTI_Teacher/Screen/Home/ViewController/HomeVC.swift
@@ -20,6 +20,22 @@ class HomeVC: BaseViewController {
             $0.backgroundColor = .main
         }
     
+    private let classProgressCV = UICollectionView(frame: .zero,
+                                                   collectionViewLayout: UICollectionViewLayout())
+        .then {
+            let layout = UICollectionViewFlowLayout()
+            layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+            layout.minimumLineSpacing = 0
+            layout.minimumInteritemSpacing = 0
+            layout.scrollDirection = .horizontal
+            
+            $0.collectionViewLayout = layout
+            $0.showsHorizontalScrollIndicator = false
+            $0.backgroundColor = .clear
+        }
+    
+    private let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -28,6 +44,7 @@ class HomeVC: BaseViewController {
         super.configureView()
         configureContentView()
         configureNaviBar()
+        configureClassProgressCV()
     }
     
     override func layoutView() {
@@ -49,12 +66,19 @@ class HomeVC: BaseViewController {
 
 extension HomeVC {
     private func configureContentView() {
-        view.addSubviews([headerView])
+        view.addSubviews([headerView,
+                          classProgressCV])
     }
     
     private func configureNaviBar() {
         naviBar.configureNaviBar(targetVC: self)
         naviBar.configureRightBarBtn(image: UIImage(named: "alarm")!)
+    }
+    
+    private func configureClassProgressCV() {
+        classProgressCV.register(ClassProgressCVC.self, forCellWithReuseIdentifier: ClassProgressCVC.className)
+        classProgressCV.dataSource = self
+        classProgressCV.delegate = self
     }
 }
 
@@ -65,6 +89,12 @@ extension HomeVC {
         headerView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(184)
+        }
+        
+        classProgressCV.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(34)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(74)
         }
     }
 }
@@ -79,4 +109,28 @@ extension HomeVC {
 
 extension HomeVC {
     
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension HomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ClassProgressCVC.className,
+                                                            for: indexPath) as? ClassProgressCVC
+        else { fatalError() }
+        cell.setClassProgress()
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewFlowLayout
+
+extension HomeVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 78, height: 74)
+    }
 }
