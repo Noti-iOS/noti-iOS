1fbbeba465b0dbfdf3d898725288e94125c18f56
yunkyung-Hwang <dudhkd98@naver.com>
2022-11-21 20:43:50 +0900

    NOTI-26/알림 설정 화면 구현

--- NOTI_Teacher/NOTI_Teacher/Screen/Home/ViewController/NotificationSettingVC.swift
+++ NOTI_Teacher/NOTI_Teacher/Screen/Home/ViewController/NotificationSettingVC.swift
@@ -15,6 +15,17 @@ import RxSwift
 class NotificationSettingVC: BaseViewController {
     private let naviBar = NavigationBar()
     
+    private let baseStackView = UIStackView()
+        .then {
+            $0.axis = .vertical
+        }
+    
+    private var focusStudentsView = ToggleStatusView()
+    
+    private var incompleteStudentsView = ToggleStatusView()
+    
+    private var disturbTimeView = ArrowStackComponentView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,10 +33,12 @@ class NotificationSettingVC: BaseViewController {
     override func configureView() {
         super.configureView()
         configureNaviBar()
+        configureContentView()
     }
     
     override func layoutView() {
         super.layoutView()
+        configureLayout()
     }
     
     override func bindInput() {
@@ -47,12 +60,38 @@ extension NotificationSettingVC {
                                  title: "알림 설정")
         naviBar.configureBackBtn()
     }
+    
+    private func configureContentView() {
+        view.addSubview(baseStackView)
+        [focusStudentsView, incompleteStudentsView, disturbTimeView].forEach {
+            baseStackView.addArrangedSubview($0)
+        }
+        baseStackView.addHorizontalSeparators()
+        
+        focusStudentsView.configureTitle("집중 관리 학생")
+        incompleteStudentsView.configureTitle("숙제 미완료 학생")
+        disturbTimeView.configureContentView(title: "방해 금지 시간")
+        // TODO: - 서버 연결 후 수정
+        disturbTimeView.configureMessage("오후 11 : 00 - 오전 10 : 00  알림 미발송")
+    }
 }
 
 // MARK: - Layout
 
 extension NotificationSettingVC {
-    
+    private func configureLayout() {
+        baseStackView.snp.makeConstraints {
+            $0.top.equalTo(naviBar.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+        
+        [focusStudentsView, incompleteStudentsView, disturbTimeView].forEach {
+            $0.snp.makeConstraints {
+                $0.height.equalTo(72)
+            }
+        }
+    }
 }
 
 // MARK: - Input
