bfc1790626e46c08bbdb2952351fca0953cdad15
yunkyung-Hwang <dudhkd98@naver.com>
2022-11-21 20:43:50 +0900

    NOTI-26/알림 설정 화면 구현

---
+++ NOTI_Teacher/NOTI_Teacher/Screen/Home/View/ToggleStatusView.swift
@@ -0,0 +1,95 @@
+//
+//  ToggleStatusView.swift
+//  NOTI_Teacher
+//
+//  Created by 황윤경 on 2022/11/19.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+
+class ToggleStatusView: BaseView {
+    private var baseStackView = UIStackView()
+        .then {
+            $0.axis = .vertical
+            $0.spacing = 4
+        }
+    
+    private var statusTitle = UILabel()
+        .then {
+            $0.font = .notoSansKR_Bold(size: 14)
+        }
+    
+    private var statusLabel = UILabel()
+        .then {
+            $0.font = .notoSansKR_Light(size: 12)
+            $0.textColor = .gray02
+        }
+    
+    private var toggleBtn = UISwitch()
+        .then {
+            $0.onTintColor = .main
+            $0.tintColor = .red
+        }
+    
+    private let bag = DisposeBag()
+    
+    override func configureView() {
+        super.configureView()
+        addViews()
+        bindToggle()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        configureLayout()
+    }
+    
+}
+
+// MARK: - Configure
+
+extension ToggleStatusView {
+    private func addViews() {
+        addSubviews([baseStackView,
+                     toggleBtn])
+        [statusTitle, statusLabel].forEach {
+            baseStackView.addArrangedSubview($0)
+        }
+    }
+    
+    func configureTitle(_ title: String) {
+        statusTitle.text = title
+    }
+}
+
+// MARK: - Layout
+
+extension ToggleStatusView {
+    private func configureLayout() {
+        baseStackView.snp.makeConstraints {
+            $0.centerY.leading.equalToSuperview()
+        }
+        
+        toggleBtn.snp.makeConstraints {
+            $0.centerY.trailing.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Input
+
+extension ToggleStatusView {
+    private func bindToggle() {
+        toggleBtn.rx.isOn
+            .asDriver()
+            .drive(onNext: {[weak self] status in
+                guard let self = self else { return }
+                self.statusLabel.text = status ? "ON" : "OFF"
+            })
+            .disposed(by: bag)
+    }
+}
---
+++ NOTI_Teacher/NOTI_Teacher/Screen/Share/ArrowStackComponentView.swift
@@ -0,0 +1,83 @@
+//
+//  ArrowStackComponentView.swift
+//  NOTI_Teacher
+//
+//  Created by 황윤경 on 2022/11/21.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class ArrowStackComponentView: BaseView {
+    private var baseStackView = UIStackView()
+        .then {
+            $0.axis = .vertical
+            $0.spacing = 4
+        }
+    
+    private var viewTitle = UILabel()
+        .then {
+            $0.font = .notoSansKR_Bold(size: 14)
+            $0.textColor = .black
+        }
+    
+    private var viewMessage = UILabel()
+        .then {
+            $0.font = .notoSansKR_Light(size: 12)
+            $0.textColor = .gray02
+        }
+    
+    private var arrowImageView = UIImageView()
+        .then {
+            $0.image = UIImage(named: "arrow_right")
+        }
+    
+    override func configureView() {
+        super.configureView()
+        addViews()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        configureLayout()
+    }
+    
+}
+
+// MARK: - Configure
+
+extension ArrowStackComponentView {
+    private func addViews() {
+        addSubviews([baseStackView,
+                     arrowImageView])
+        [viewTitle, viewMessage].forEach {
+            baseStackView.addArrangedSubview($0)
+        }
+    }
+    
+    func configureContentView(title: String, message: String = "") {
+        viewTitle.text = title
+        viewMessage.text = message
+    }
+    
+    func configureMessage(_ message: String) {
+        viewMessage.text = message
+    }
+}
+
+// MARK: - Layout
+
+extension ArrowStackComponentView {
+    private func configureLayout() {
+        baseStackView.snp.makeConstraints {
+            $0.centerY.leading.equalToSuperview()
+        }
+        
+        arrowImageView.snp.makeConstraints {
+            $0.leading.equalTo(baseStackView.snp.trailing).offset(8)
+            $0.centerY.trailing.equalToSuperview()
+            $0.width.height.equalTo(38)
+        }
+    }
+}
